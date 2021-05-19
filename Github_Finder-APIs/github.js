3e9a89cb5c58c78d637710d9e7ec3617fdfca63b
anurag-shukla8004 <79192104+anurag-shukla8004@users.noreply.github.com>
2021-05-19 20:18:12 +0530

    Add files via upload

---
+++ Github_Finder-APIs/github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'ec6d0d1b1c19e58bfcf9';
+    this.client_secret = '3261220d592e68e304261c189f80e21656a5cd25';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
